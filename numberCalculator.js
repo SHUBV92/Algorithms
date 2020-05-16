e910bb009883006e257336df4f1b6831adb8aa70
your Shubinder Virk <shubinder92@gmail.com>
2020-05-16 17:41:31 +0100

    Added Roman Numeral Objects - Trial & Error

--- numberCalculator.js
+++ numberCalculator.js
@@ -3,15 +3,18 @@ numbers = [
     name: "Ten",
     digit: 10
   },
-
   {
     name: "hundreds",
     digit: 100
   },
   {
-    thousand: 1000
+    name:"thousand",
+    digit: 1000
   },
-  { name: "Ten-Thousand", digit: 10000 },
+  { 
+      name: "Ten-Thousand", 
+      digit: 10000 
+  },
   {
     name: "Hundred Thousand",
     digit: 100000
@@ -27,15 +30,18 @@ numbers = [
 ];
 
 const calculate = number => {
-  
-//     numbers.filter()
-//     if (number ===  numbers[i].) {
-//     return number[i]
-//   }
+  //     numbers.filter()
+  //     if (number ===  numbers[i].) {
+  //     return number[i]
+  //   }
 
-document.getElementById(result) = numbers[0].name
+  let result = document.getElementById("result");
+  console.log(result);
+  let result = 0;
+  // numbers[0].name
 
   return numbers;
 };
 
 console.log(calculate(1000));
+console.log("Number: ", numbers[0].name);
---
+++ romanNumerals.js
@@ -0,0 +1,80 @@
+numbers = [
+  {
+    name: " One ",
+    roman: "I",
+    digit: 1
+  },
+
+  {
+    name: " Two ",
+    roman: "II",
+    digit: 2
+  },
+
+  {
+    name: " Three ",
+    roman: "III",
+    digit: 3
+  },
+
+  {
+    name: " Five ",
+    roman: "V",
+    digit: 5
+  },
+
+  {
+    name: " Ten ",
+    roman: "X",
+    digit: 10
+  },
+
+  {
+    name: " Fifty ",
+    roman: "L",
+    digit: 50
+  },
+  {
+    name: "Hundred",
+    roman: "C",
+    digit: 100
+  },
+  {
+    name: "Five - Hundred",
+    roman: "D",
+    digit: 500
+  },
+  {
+    name: "One Thousand",
+    roman: "M",
+    digit: 1000
+  },
+  {
+    name: "One Million",
+    roman: "X",
+    digit: 1000000
+  },
+  {
+    name: "Ten Million",
+    roman: "X",
+    digit: 10000000
+  }
+];
+
+
+// five
+// If under five --> then the counting numbers will go before the primary numbers 
+
+
+// Ten 
+// if 
+
+// Hundred 
+
+
+// Five Hundred 
+// If Hundred Less 
+
+
+// if Under 1000
+// 
